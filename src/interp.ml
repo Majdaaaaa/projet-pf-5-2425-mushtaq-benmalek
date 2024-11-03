1216371ef16c513b4367496e2a2a5170002fbed6
Majda <150187123+Majdaaaaa@users.noreply.github.com>
2024-11-03 14:54:18 +0100

    ajout travail amenah + exo 4.6

--- src/interp.ml
+++ src/interp.ml
@@ -1,5 +1,4 @@
 open Geo
-
 (* Code de la Section 4 du projet. *)
 
 type instruction =
@@ -8,46 +7,92 @@ type instruction =
 | Either of program * program 
 and program = instruction list
 
-let is_deterministic (prog : program) : bool =
-  failwith "À compléter"
+let rec is_deterministic (prog : program) : bool =
+  match prog with
+  | [] -> true
+  | a::w -> match a with
+            | Move t -> is_deterministic w
+            | Repeat (i,p) -> is_deterministic (p@w)
+            | Either (p,q) -> false
+;;
+exception E
+(* Réponse à l'exercice 4.1 *)
+(* exception E
+let rec unfold_repeat (prog : program) : program =
+  match prog with
+  | [] -> []
+  | a::w ->
+     match a with
+            | Move t -> Move t :: unfold_repeat w
+            | Repeat (i,p) ->
+              if i=0 then unfold_repeat w
+                else p@unfold_repeat(Repeat (i-1,p)::w)
+              
+            | Either(p,q) -> raise E
+;; *)
 
-let unfold_repeat (prog : program) : program =
-  failwith "À compléter"
-
-let run_det (prog : program) (p : point) : point list =
-  failwith "À compléter"
-
-let target_reached_det (prog : program) (p : point) (target : rectangle) : bool =
-  failwith "À compléter"
-  
-let rec run (prog : program) (p : point) : point list =
-  match prog with 
-  [] -> [p]
-  |ins :: r ->
-    match ins with 
-    Move t ->
-      let new_point = transform t p in 
-      new_point :: run r new_point
-    |Repeat(n,r_prog) -> 
-      let rec aux (n : int) (prog : program) (p : point) (pl : point list) : point list =
-        if n > 0 then
-          match prog with 
-          [] -> aux (n-1) prog p pl 
-          |ins::r ->
-            match ins with 
-            Move t -> 
-              let new_point = transform t p in 
-              aux n r new_point (pl @ [new_point])
-          |Repeat(n1,r1_prog) -> aux n r1_prog  p (pl @ aux n1 r1_prog p [])
-          |Either(p1,p2)-> aux n (if Random.bool () then p1 else p2) p pl
-          else pl
-      in aux n r_prog p []
-    |Either(p1,p2) -> if Random.bool () then run (p1 @ r) p else run (p2 @ r) p
+(* Réponse à l'exercice 4.4 *)
+let rec unfold_repeat (prog : program) : program =
+  match prog with
+  | [] -> []
+  | a::w ->
+     match a with
+            | Move t -> Move t :: unfold_repeat w
+            | Repeat (i,p) ->
+              if i=0 then unfold_repeat w
+                else p@unfold_repeat(Repeat (i-1,p)::w)
+              
+            | Either(p,q) -> unfold_repeat(p@q@w) 
 ;;
 
+let run_det (prog : program) (p : point) : point list =
+  let rec aux prog p =
+    match prog with
+    | [] -> []
+    | a::w ->
+      match a with
+      | Move t -> let new_point = transform t p  in new_point:: aux w new_point
+      | Repeat (i,pro) -> aux (unfold_repeat prog) p
+      | Either (p,q) -> raise E
+    in p::aux prog p
+  ;;
 
-let all_choices (prog : program) : program list =
-  failwith "À compléter"
+
+let target_reached_det (prog : program) (p : point) (target : rectangle) : bool =
+  let l=run_det prog p in
+  in_rectangle target (List.nth l ((List.length l)-1))
+  
+let run (prog : program) (p : point) : point list =
+  let rec aux prog p =
+    match prog with
+    | [] -> []
+    | a::w -> 
+      match a with
+      | Move t -> let new_point= transform t p in new_point::aux w new_point
+      | Repeat(i,pro) -> aux (unfold_repeat prog) p
+      | Either(pp,q) -> let b=Random.bool() in 
+                      if b then aux (pp@w) p
+                      else aux(q@w) p 
+    in p::aux prog p
+  ;;
+
+  let rec all_choices (prog : program) : program list =
+    match prog with 
+    | [] -> [[]]
+    | ins :: r ->
+      match ins with
+      | Repeat (i, p) -> 
+          let unfolded_prog = unfold_repeat [Repeat (i, p)] in
+          all_choices (unfolded_prog @ r) (* ajoute des instruction a la liste des programme 
+                                          * Donc quand les instruction arrivrant a la branche Move t 
+                                          * Ils seront ajouté a chaque branche du programme *)
+      | Either (p1, p2) -> 
+          let choices_p1 = all_choices (p1 @ r) in
+          let choices_p2 = all_choices (p2 @ r) in
+          choices_p1 @ choices_p2
+      | Move t -> List.map (fun p -> Move t :: p) (all_choices r) (* ajouter Move t a chaque branche du programme *)
+;;
 
 let target_reached (prog : program) (p : point) (r : rectangle) : bool =
-  failwith "À compléter"
+  
+;;
